b1d91c1d627f1111373a40893fa8f84ecdee8d88
unknown <holmanryan4@gmail.com>
2020-02-01 17:35:59 -0600

    i have laid the foundation for this project

---
+++ Sweepstakes/MarketingFirm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class MarketingFirm
+    {
+
+        //Member Variables (HAS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+
+        //Member Variables (HAS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
